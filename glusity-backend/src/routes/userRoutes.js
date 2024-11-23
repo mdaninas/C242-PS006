a73026faabb7a4367881b9b03836c88c89d90b7f
mdaninas <mdaninas@gmail.com>
2024-11-23 20:55:54 +0700

    penghapusan file

--- glusity-backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.get('/profile', authMiddleware, userController.getProfile);
-router.put('/update', authMiddleware, userController.updateUser);
-
-module.exports = router;
